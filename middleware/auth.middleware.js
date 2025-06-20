74ca23e12ad03002f6cd4ef92c5675e160a5979b
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2025-06-20 18:18:31 +0530

    add authorization middleware for user retrieval

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,42 @@
+import {JWT_SECRET} from '../config/env.js';
+import User from '../models/user.model.js';
+import jwt from 'jsonwebtoken';
+
+const authorize = async (req, res, next) => {
+    try{
+        let token;
+
+        if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
+            token = req.headers.authorization.split(" ")[1];
+        }
+        
+
+        if(!token){
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized, no token provided"
+            })
+        }
+        // Verify token
+        const decoded = jwt.verify(token, JWT_SECRET);
+        const user = await User.findById(decoded.userId).select("-password -__v");
+
+        if(!user){
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized, user not found"
+            });
+        }
+
+        req.user = user;
+        next();
+    }  
+    catch(error){
+        res.status(403).json({
+            success:false,
+            message: "Unauthorized"
+        })
+    }
+}
+
+export default authorize;
